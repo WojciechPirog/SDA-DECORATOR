d38fa78ad572fcd32e7782104fd27d8a28153d3b
monikape <monika.pluska@wp.pl>
2018-03-26 16:33:43 +0200

    dodanie klays:RedShapeDecorator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Shape circle = new Circle();
 
+        Shape redCircle = new RedShapeDecorator(new Circle());
+
+        Shape redRectangle = new RedShapeDecorator(new Rectangular());
+        System.out.println("Circle with normal border");
+        circle.draw();
+
+        System.out.println("\nCircle of red border");
+        redCircle.draw();
+
+        System.out.println("\nRectangle of red border");
+        redRectangle.draw();
     }
+
 }
